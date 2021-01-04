498f85348adfe71bf88c8c7f8b9250b7b28d9d78
Shrimp0927 <nguyenhaidang123123@gmail.com>
2021-01-03 22:53:46 -0800

    new landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,10 +3,18 @@ import React from 'react';
 const Landing = (props) => {
 	return (
 		<div>
-            <button style={{ margin: '0', position: 'absolute', top: '50%', left: '45%' }}>
-                <a
-                href="/auth/google" 
-                style={{textDecoration: 'none', color: 'black'}}>Login with Google</a>
+            <h1 style={{ margin: '0', position: 'absolute', top: '35%', left: '28%' }}>
+                Welcome to Imaghut!
+            </h1>
+            <h4 style={{ margin: '0', position: 'absolute', top: '50%', left: '47%' }}>
+                Share photos for free!
+            </h4>
+            <button style={{
+                margin: '0', 
+                position: 'absolute', 
+                top: '60%', 
+                left: '61%' }}>
+                <a style={{color:'black'}} href="/auth/google">Login with google</a>
             </button>
 		</div>
 	);
